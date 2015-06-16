bde823b45c912e1685a44ebf392024ba690e6177
Long Nguyen <olragon@gmail.com>
2015-06-16 21:25:04 +0700

    Run composer install and create schema.

---
+++ scripts/Deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Set php version through phpenv. 5.3, 5.4 and 5.5 available
+phpenv local 5.4
+# Install extensions through Pecl
+# yes yes | pecl install memcache
+
+# Install dependencies through Composer
+composer install --prefer-source --no-interaction
+
+# Processes the schema and either create it
+php cli.php orm:schema-tool:create
+
